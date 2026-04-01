53db0820084278fb67fc5f70df8f7e1480c0e68c
KCJ <kcj0702@gmail.com>
2026-04-01 21:19:41 +0900

    아몰랑 추가

---
+++ 05_플라스크/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template()
+
+
+@app.route("/hello")
+def hello():
+    return "만나서 반갑습니다 응애"
+
+
+@app.route("/user/<userId>")
+def profile(userId):
+    return f"{userId}\' profile"
+
+
+if __name__ == "__main__":
+    app.run()
