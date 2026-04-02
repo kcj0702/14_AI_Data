ecbd4fe172b8fc5d924c16185d6d57e01b006953
KCJ <kcj0702@gmail.com>
2026-04-02 21:04:03 +0900

    크롤링

--- 05_플라스크/app.py
+++ 05_플라스크/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template()
+    return render_template("index.html")
 
 
 @app.route("/hello")
@@ -18,4 +18,4 @@ def profile(userId):
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
